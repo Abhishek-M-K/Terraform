34e9019d32faed3e55b2eeeb397300720b6d4ff6
Abhishek-M-K <khandareabhishek98@gmail.com>
2025-01-03 02:07:00 +0530

    chore: terraform variables

---
+++ 03-web-app/terraform.tfvars
@@ -0,0 +1,7 @@
+# This file contains the variables that are used in the Terraform configuration files.
+
+aws_region = "us-east-1"
+db_name = "sampledb"
+db_username = "admin"
+domain_name = "abhishekkhandare.dev"
+#db_password will be applied via runtime input
--- 03-web-app/variables.tf
+++ 03-web-app/variables.tf
@@ -1,3 +1,45 @@
+# Parameterized variables
+
+# Region
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+  type = string
+  default = "us-east-1"
+}
+
+# AMI
+variable "ami" {
+  description = "The AMI to use for the EC2 instance"
+  type = string
+  default = "ami-011899242bb902164"
+}
+
+# Instance type
+variable "instance_type" {
+  description = "The instance type to use for the EC2 instance"
+  type = string
+  default = "t2.micro"
+}
+
+# AWS Route53 variables -> Domain | DNS
+variable "domain_name" {
+  description = "The domain name to use for the Route53 hosted zone"
+  type = string
+}
+
+
+
+# AWS RDS variables
+variable "db_name" {
+  description = "The sample rds instance name"
+  type = string
+}
+
+variable "db_username" {
+  description = "The sample rds instance username"
+  type = string
+}
+
 variable "db_password" {
   description = "The sample rds instance password"
   type = string
